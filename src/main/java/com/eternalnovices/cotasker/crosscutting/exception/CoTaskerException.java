ec1b32a1d97e1b06a7971d1d6d9cc57083bf7b08
Eliudcampos <131806393+Eliudcampos@users.noreply.github.com>
2023-10-04 15:16:57 -0500

    Add files via upload
    Crosscuting exceptions

---
+++ src/main/java/com/eternalnovices/cotasker/crosscutting/exception/CoTaskerException.java
@@ -0,0 +1,7 @@
+package com.eternalnovices.cotasker.crosscutting.exception;
+
+public class CoTaskerException extends RuntimeException {
+
+	private static final long serialVersionUID = -8043648350173333618L;
+
+}
