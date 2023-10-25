f1bd350c8535a4bd80271f4c907a1fb74904a072
Andres Ochoa <andresochoaflorez0@gmail.com>
2023-10-24 23:22:00 -0500

    se crean Domain

---
+++ src/main/java/com/eternalnovices/cotasker/service/domain/usuario/UsuarioDomain.java
@@ -0,0 +1,70 @@
+package com.eternalnovices.cotasker.service.domain.usuario;
+
+import java.util.UUID;
+
+public class UsuarioDomain {
+	private UUID idUsuario;
+	private String nombre;
+	private String apellido;
+	private String correoElectronico;
+	private boolean correoElectronicoConfirmado;
+	private String contrasena;
+	
+	private UsuarioDomain(final UUID idUsuario,final String nombre,final String apellido,final String correoElectronico,
+			final boolean correoElectronicoConfirmado,final String contrasena) {
+		
+		setId(idUsuario);
+		setNombre(nombre);
+		setApellido(apellido);
+		setCorreoElectronico(correoElectronico);
+		setCorreoElectronicoConfirmado(correoElectronicoConfirmado);
+		setContrasena(contrasena);
+	}
+	
+	public static final  UsuarioDomain crear(final UUID idUsuario, final String nombre,final String apellido,final String correoElectronico,
+			final boolean correoElectronicoConfirmado,final String contrasena  ) {
+		
+		return  new UsuarioDomain (idUsuario ,nombre,apellido,correoElectronico,correoElectronicoConfirmado,contrasena);
+	}
+	
+	public final UUID getId() {
+		return idUsuario;
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	public final String getApellido() {
+		return apellido;
+	}
+	public final String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	public final boolean isCorreoElectronicoConfirmado() {
+		return correoElectronicoConfirmado;
+	}
+	public final String getContrasena() {
+		return contrasena;
+	}
+	private final void setId(final UUID id) {
+		this.idUsuario = id;
+	}
+	private final void setNombre(final String nombre) {
+		this.nombre = nombre;
+	}
+	private final void setApellido(final String apellido) {
+		this.apellido = apellido;
+	}
+	private final void setCorreoElectronico(final String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	private final void setCorreoElectronicoConfirmado( final boolean correoElectronicoConfirmado) {
+		this.correoElectronicoConfirmado = correoElectronicoConfirmado;
+	}
+	private final void setContrasena(final String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
+	
+	
+}
