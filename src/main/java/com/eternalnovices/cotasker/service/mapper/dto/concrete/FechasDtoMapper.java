2d9a7bba5ec6b8ae37f4319be5e669406dafc0d5
Andres Ochoa <andresochoaflorez0@gmail.com>
2023-10-26 03:42:46 -0500

    se crean DtoMapper

---
+++ src/main/java/com/eternalnovices/cotasker/service/mapper/dto/concrete/FechasDtoMapper.java
@@ -0,0 +1,13 @@
+package com.eternalnovices.cotasker.service.mapper.dto.concrete;
+
+import com.eternalnovices.cotasker.service.domain.fechas.FechasDomain;
+import com.eternalnovices.cotasker.service.dto.FechasDTO;
+
+public class FechasDtoMapper {
+
+	public static FechasDomain convertToDomain(FechasDTO fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
