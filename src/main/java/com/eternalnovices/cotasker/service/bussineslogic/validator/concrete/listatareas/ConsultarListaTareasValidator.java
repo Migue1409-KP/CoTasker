3c19448e074e0b894a022d334e48eb0de34af808
Eliudcampos <elicampos321@gmail.com>
2023-10-27 03:36:55 -0500

    Consultar Lista/tarea Validator

--- src/main/java/com/eternalnovices/cotasker/service/bussineslogic/validator/concrete/listatareas/ConsultarListaTareasValidator.java
+++ src/main/java/com/eternalnovices/cotasker/service/bussineslogic/validator/concrete/listatareas/ConsultarListaTareasValidator.java
@@ -1,13 +1,19 @@
 package com.eternalnovices.cotasker.service.bussineslogic.validator.concrete.listatareas;
 
+import com.eternalnovices.cotasker.crosscutting.util.UtilFecha;
 import com.eternalnovices.cotasker.crosscutting.util.UtilObjeto;
 import com.eternalnovices.cotasker.crosscutting.util.UtilTexto;
 import com.eternalnovices.cotasker.crosscutting.util.UtilUUID;
 import com.eternalnovices.cotasker.service.bussineslogic.validator.Validator;
+import com.eternalnovices.cotasker.service.domain.estado.rules.IdEstadoValidationRule;
+import com.eternalnovices.cotasker.service.domain.fechas.rules.FechaCreacionFechasValidationRule;
+import com.eternalnovices.cotasker.service.domain.fechas.rules.FechasValidationRule;
 import com.eternalnovices.cotasker.service.domain.listatareas.ListaTareasDomain;
 import com.eternalnovices.cotasker.service.domain.listatareas.rules.DescripcionListaTareasValitationRule;
 import com.eternalnovices.cotasker.service.domain.listatareas.rules.IdListaTareasValidationRule;
 import com.eternalnovices.cotasker.service.domain.listatareas.rules.NombreListaTareasValitadionRule;
+import com.eternalnovices.cotasker.service.domain.prioridad.rules.IdPrioridadValidationRule;
+import com.eternalnovices.cotasker.service.domain.proyecto.rules.IdProyectoValidationRule;
 import com.eternalnovices.cotasker.service.domain.tarea.TareaDomain;
 import com.eternalnovices.cotasker.service.domain.tarea.rules.DescripcionTareaValitationRule;
 import com.eternalnovices.cotasker.service.domain.tarea.rules.IdTareaValidationRule;
@@ -35,8 +41,25 @@ public final class ConsultarListaTareasValidator implements Validator<ListaTarea
 				NombreListaTareasValitadionRule.ejecutarValidacion(domain.getNombre());
 			}
 			
-			if(!UtilTexto.estaVacio(domain.getNombre())) {				
+			if(!UtilTexto.estaVacio(domain.getDescripcion())) {				
 				DescripcionListaTareasValitationRule.ejecutarValidacion(domain.getDescripcion());
+			}
+			
+			if(!UtilObjeto.esNulo(domain.getFecha())) {
+				if(!UtilFecha.esNulo(domain.getFecha().getFechaCreacion())) {
+					FechaCreacionFechasValidationRule.ejecutarValidacion(domain.getFecha().getFechaCreacion());
+					FechasValidationRule.ejecutarValidacion(domain.getFecha());
+				}
+			}
+			
+			if(!UtilObjeto.esNulo((domain.getPrioridad()))) {
+				IdPrioridadValidationRule.ejecutarValidacion(domain.getPrioridad().getIdPrioridad());
+			}
+			
+		
+			
+			if(!UtilObjeto.esNulo((domain.getProyecto()))) {
+				IdProyectoValidationRule.ejecutarValidacion(domain.getProyecto().getIdProyecto());
 			}
 		}
 	}
--- src/main/java/com/eternalnovices/cotasker/service/bussineslogic/validator/concrete/tarea/ConsultarTareaValidator.java
+++ src/main/java/com/eternalnovices/cotasker/service/bussineslogic/validator/concrete/tarea/ConsultarTareaValidator.java
@@ -1,9 +1,15 @@
 package com.eternalnovices.cotasker.service.bussineslogic.validator.concrete.tarea;
 
+import com.eternalnovices.cotasker.crosscutting.util.UtilFecha;
 import com.eternalnovices.cotasker.crosscutting.util.UtilObjeto;
 import com.eternalnovices.cotasker.crosscutting.util.UtilTexto;
 import com.eternalnovices.cotasker.crosscutting.util.UtilUUID;
 import com.eternalnovices.cotasker.service.bussineslogic.validator.Validator;
+import com.eternalnovices.cotasker.service.domain.estado.rules.IdEstadoValidationRule;
+import com.eternalnovices.cotasker.service.domain.fechas.rules.FechaCreacionFechasValidationRule;
+import com.eternalnovices.cotasker.service.domain.fechas.rules.FechasValidationRule;
+import com.eternalnovices.cotasker.service.domain.listatareas.rules.IdListaTareasValidationRule;
+import com.eternalnovices.cotasker.service.domain.prioridad.rules.IdPrioridadValidationRule;
 import com.eternalnovices.cotasker.service.domain.tarea.TareaDomain;
 import com.eternalnovices.cotasker.service.domain.tarea.rules.DescripcionTareaValitationRule;
 import com.eternalnovices.cotasker.service.domain.tarea.rules.IdTareaValidationRule;
@@ -31,9 +37,30 @@ public final class ConsultarTareaValidator implements Validator<TareaDomain>{
 				NombreTareaValitadionRule.ejecutarValidacion(domain.getNombre());
 			}
 			
-			if(!UtilTexto.estaVacio(domain.getNombre())) {				
+			if(!UtilTexto.estaVacio(domain.getDescripcion())) {				
 				DescripcionTareaValitationRule.ejecutarValidacion(domain.getDescripcion());
 			}
+			
+			if(!UtilObjeto.esNulo(domain.getFecha())) {
+				if(!UtilFecha.esNulo(domain.getFecha().getFechaCreacion())) {
+					FechaCreacionFechasValidationRule.ejecutarValidacion(domain.getFecha().getFechaCreacion());
+					FechasValidationRule.ejecutarValidacion(domain.getFecha());
+				}
+			}
+			
+			if(!UtilObjeto.esNulo((domain.getPrioridad()))) {
+				IdPrioridadValidationRule.ejecutarValidacion(domain.getPrioridad().getIdPrioridad());
+			}
+			
+			if(!UtilObjeto.esNulo((domain.getEstado()))) {
+				IdEstadoValidationRule.ejecutarValidacion(domain.getEstado().getIdEstado());
+			}
+			
+			if(!UtilObjeto.esNulo((domain.getListaTareas()))) {
+				IdListaTareasValidationRule.ejecutarValidacion(domain.getListaTareas().getIdListaTareas());
+			}
+			
+			
 		}
 	}
 
